65a37591931cb8aceb2a0ad8f5b83919610a361e
anuj thapa <anujthapa@outlook.com>
2018-09-13 14:06:22 +0300

    assignment of integrify

---
+++ sprint1/javascript assignment/main.js
@@ -0,0 +1,68 @@
+console.log("exercise 1---------------------------");
+
+console.log("variables");
+console.log("                                   ");
+
+console.log("1.Declare variables to store your first name, last name, marital status, country and age in multiple lines");
+
+let fName="anuj";
+let lName="thapa";
+let maritialStatus="married";
+let country="Nepal";
+let age=25;
+console.log("                                   ");
+console.log("b.Declare variables to store your first name, last name,  marital status, country and age in a single line");
+
+let fname,lname,maritialstatus,Country,Age;
+console.log("                                   ");
+console.log("c.Declare two variables myAge and yourAge and assign them initial values and log to browser console.");
+ let myAge=25;
+ let yourAge=30;
+
+ console.log(`I am ${myAge} years old`);
+ console.log(`You are ${yourAge} years old`);
+ console.log("                                   ");
+ console.log("2.datatypes");
+ console.log("                                   ");
+
+ console.log("The JavaScript typeof operator uses to check different data types. Check the data type of each variables from question number 1.");
+ 
+ console.log("");
+ console.log(typeof(fName));
+ console.log(typeof(lName));
+ console.log(typeof(maritialStatus));
+ console.log(typeof(country));
+ console.log(typeof(age));
+ console.log(typeof(myAge));
+ console.log(typeof(yourAge));
+ 
+
+ console.log("");
+ console.log("3.String");
+ console.log();
+ console.log("Declare a variable name company and assign it to an initial value “Integrify Academy”.");
+ let company = "Integrify Acadamy";
+ console.log("Print the string  on the browser console using console.log()");
+ console.log( );
+console.log(company);
+
+console.log();
+console.log("Print the length of the string  on the browser console using console.log()");
+console.log(company.length);
+
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+ 
+
+ 
+ 
+
+ 
+ 
